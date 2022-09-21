a91152aeb8d484e2d38d3017b2197b408aea61dc
kaymakebru <ebru.kymk72@gmail.com>
2022-09-21 17:30:19 +0300

    JavaHomework

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,21 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 12;
+		sayi = 1311111111;
+		
+		byte sayi2 = 12;
+		sayi2 = 127;
+		
+		double sayi3 = 12.5;
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+		
+		
+	}
+
+}
